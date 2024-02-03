dd3a958531a38f0e3b91593dc0cfd76f80eaa012
unknown <vlfmap@gmail.com>
2024-02-03 20:44:49 +0100

    digits all fixed

--- CLOOCKWORK/CLOOCKWORK.ino
+++ CLOOCKWORK/CLOOCKWORK.ino
@@ -23,12 +23,12 @@ void setup() {
   pinMode(CLOCK, OUTPUT);
   URTCLIB_WIRE.begin();
   Serial.begin(9600);
-  rtc.set(0, 13, 17, 7, 2, 5, 15);
+  rtc.set(0, 34, 20, 7, 2, 5, 15);
   Serial.println("All set");
 }
 
 void loop() {
-  static const unsigned long displayRefreshInterval = 250;
+  static const unsigned long displayRefreshInterval = 1000;
   static const unsigned long clockRefreshInterval = 500;
   static unsigned long lastRefreshTime = 0;
   static unsigned long lastClockReTime = 0;
@@ -46,7 +46,7 @@ void loop() {
 
 /* Updates Display */
 void updateDisplay() {
-  getBackFourDigits(rtc.second(), rtc.second(), numbers);
+  getBackFourDigits(hours, minutes, numbers);
   setDisplayMemory(numbers[0], numbers[1], numbers[2], numbers[3]);
   writeDisplayRegisters();
   writeDisplayRegisters();
@@ -78,19 +78,22 @@ void setDisplayMemory(int firstDigit, int secondDigit, int thirdDigit, int fourt
   int rDigits[4] = { firstDigit, secondDigit, thirdDigit, fourthDigit };
 
   bool characterMap[10][8] = {
-    { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW },
+    { 1, 1, 1, 1, 1, 1, 0, 0 },
     { 0, 0, 0, 1, 1, 0, 0, 0 },
-    { 0, 1, 1, 0, 1, 1, 0, 0 },
-    { 0, 0, 1, 1, 1, 1, 1, 0 },
+    { 0, 1, 1, 0, 1, 1, 1, 0 },
+    { 0, 0, 1, 1, 1, 1, 1, 1 },
     { 1, 0, 0, 1, 1, 0, 1, 0 },
     { 1, 0, 1, 1, 0, 1, 1, 0 },
     { 1, 1, 1, 1, 0, 1, 1, 0 },
     { 0, 0, 0, 1, 1, 1, 1, 0 },
-    { 1, 1, 1, 1, 1, 1, 0, 0 },
+    { 1, 1, 1, 1, 1, 1, 1, 0 },
     { 1, 0, 1, 1, 1, 1, 1, 0 }
   };
   //  6, 5, 4, 3, 2, 1, x, 7
  // { 0, 1, 1, 0, 1, 1, 0, 0 },
+
+ // 0 { 1, 1, 1, 1, 1, 1, 1, 0 },
+ // 8 { 1, 1, 1, 1, 1, 1, 0, 0 },
   int displayMemoryAddress = 0;
 
   for (int r = 0; r < 4; r++) {
